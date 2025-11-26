a80f8033c14707af999694544c0caf8e8455ffda
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-26 21:26:47 +0545

    Task3 completed

---
+++ Task3/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Printer
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine($"Message: {message}");
+        }
+
+        // Print an integer number
+        public void Print(int number)
+        {
+            Console.WriteLine($"Number: {number}");
+        }
+
+        // Print a message multiple times
+        public void Print(string message, int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine($"{i}: {message}");
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Printer object
+            Printer myPrinter = new Printer();
+
+            // Call all overloaded methods
+            myPrinter.Print("Hello, World!");    
+            myPrinter.Print(2025);               
+            myPrinter.Print("Repeat this", 3);   // Print message multiple times
+        }
+    }
+}
