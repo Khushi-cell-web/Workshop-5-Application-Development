10a38f2f573873bc19b1404b0727ce6df84cfec1
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-26 22:31:56 +0545

    Task4

---
+++ Task4/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Car : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car engine started.");
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine("Car engine stopped.");
+        }
+    }
+
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bike bike = new Bike();
+
+        Console.WriteLine("Car:");
+        car.Display();
+        car.StartEngine();
+        car.StopEngine();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Bike:");
+        bike.Display();
+        bike.StartEngine();
+        bike.StopEngine();
+    }
+}
